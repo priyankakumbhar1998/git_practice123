32e4a02978b5cba9f36b7a5d4c4f2521950d5332
khachanemonika <monikakhachane03@gmail.com>
2024-06-30 10:37:38 +0530

    update code by monika

--- jwt_auth_project/auth_app/views.py
+++ jwt_auth_project/auth_app/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class UserViewSet(viewsets.ModelViewSet):
+	//git practice code added by monika
     serializer_class = UserSerializer
     queryset = User.objects.all()
     http_method_names=['post', 'get']
